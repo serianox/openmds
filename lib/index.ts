a535e1744cdb1db4e146437ae76b832c4331c619
Thomas Duboucher <thomas@duboucher.eu>
2018-02-03 10:59:36 +0100

    Make tslint happy

--- lib/index.ts
+++ lib/index.ts
@@ -1,8 +1,8 @@
-import * as Hapi from 'hapi';
+import * as Hapi from "hapi";
 
 const app = new Hapi.Server();
 
-app.connection({ port: 3000, host: 'localhost', routes: { cors: true } });
+app.connection({ port: 3000, host: "localhost", routes: { cors: true } });
 
 app.start(error => {
     if (error) {
